f1cfc935cc225ebe5ba7d0ee62e10db8317093da
Syed Ali <sali3@cox.net>
2016-10-24 16:46:58 -0400

    Assigned function value to array and consoled it to confirm

--- js/game.js
+++ js/game.js
@@ -1,16 +1,44 @@
 (function diceGame() {
 
-    // Use document.querySelector('...') to find HTML elements
-    // The argument into the function is a CSS selector!
-    // Any HTML element has an `innerText` property that you can
-    // access (or change) for the contents of that element.
+  // Use document.querySelector('...') to find HTML elements
+  // The argument into the function is a CSS selector!
+  // Any HTML element has an `innerText` property that you can
+  // access (or change) for the contents of that element.
 
-    document.querySelector('button').addEventListener('click', function buttonClicked() {
 
-        var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
-        console.log(dieNumOne);
 
-    });
+  document.querySelector('button').addEventListener('click', function buttonClicked() {
+
+    var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
+    console.log(dieNumOne);
+
+
+
+    var result = game.rollDice();
+    console.log(result);
+
+
+
+  });
+
+
+
+  var game = {
+    startDate: new Date(),
+    gameRound: [],
+    rollDice: function rollingDice () {
+      var firstDice = Math.ceil(Math.random() * 6);
+      var secondDice = Math.ceil(Math.random() * 6);
+      return [firstDice, secondDice];
+    }
+
+
+  };
+
+
+
+
+
 
 
 })();
