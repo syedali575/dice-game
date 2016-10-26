94809c616c6de0a38810bd2277bf9cbdb9110d5e
Syed Ali <sali3@cox.net>
2016-10-26 13:53:17 -0400

    Completed roll count to win. Now I need to work on time to win component

--- js/game.js
+++ js/game.js
@@ -1,99 +1,72 @@
-  (function diceGame() {
+    (function diceGame() {
 
-    // Use document.querySelector('...') to find HTML elements
-    // The argument into the function is a CSS selector!
-    // Any HTML element has an `innerText` property that you can
-    // access (or change) for the contents of that element.
+      // Use document.querySelector('...') to find HTML elements
+      // The argument into the function is a CSS selector!
+      // Any HTML element has an `innerText` property that you can
+      // access (or change) for the contents of that element.
 
 
-
-    document.querySelector('button').addEventListener('click', function buttonClicked() {
-
-      // Storing random dice numbers into array result
-
-      var result = game.rollDice();
-
-      // var dieNumOne = document.querySelector('.dice aside:first-child').innerText;
-      document.querySelector('.dice aside:first-child').innerText = result[0];
-      document.querySelector('.dice aside:last-child').innerText = result[1];
+      // Creation of Object
+      var game = {
+        startDate: new Date,
+        gameRounds: [  { roll:0, time:new Date() }  ],
+        rollDice: function rollingDice () {
+          var firstDice = Math.ceil(Math.random() * 6);
+          var secondDice = Math.ceil(Math.random() * 6);
+          return [firstDice, secondDice];
+        }
+      };
 
       // Game start time stamp
       var startTime = game.startDate.getTime();
-      console.log(startTime);
-
-      // Game end time stamp
-      var endTime = game.gameRounds[game.gameRounds.length -1].time.getTime();
-      console.log(endTime);
-
-      // time to win
-      var timeDiff = endTime - startTime;
-      console.log(timeDiff);
-
+      // console.log(startTime);
       document.querySelector(".game-start").innerText = "Game Started "+game.startDate.getFullYear() + "-" +(game.startDate.getMonth()+1) + " at " + startTime;
 
 
+       // Button click function
+       document.querySelector('button').addEventListener('click', function buttonClicked() {
+
+      // Storing random dice numbers into array result
+      var result = game.rollDice();
+
+        // Connecting dice boxes
+        document.querySelector('.dice aside:first-child').innerText = result[0];
+        document.querySelector('.dice aside:last-child').innerText = result[1];
 
 
-      // Tracking number of rolls to win
-
-      game.gameRounds[game.gameRounds.length - 1].roll += 1;
-
-      console.log(game.gameRounds[game.gameRounds.length - 1].roll);
-
-        // Tracking time to win
-
-        game.gameRounds[game.gameRounds.length -1].time.getTime();
-
-        console.log(game.gameRounds[game.gameRounds.length -1].time.getTime());
+        // Game end time stamp
+        var endTime = game.gameRounds[game.gameRounds.length -1].time.getTime();
+        console.log(endTime);
 
 
+        // Tracking number of rolls to win
+        game.gameRounds[game.gameRounds.length - 1].roll += 1;
+        // console.log(game.gameRounds[game.gameRounds.length - 1].roll);
 
-      // Winner calculation Logic
+          // time to win
+          var timeDiff = endTime - startTime;
+          console.log(timeDiff);
 
-      if ((result[0] + result[1]) === 7) {
-        // console.log("Winner");
-        document.querySelector("h2").innerText = "Winner";
-
-
-        document.querySelector(".message").innerText = "It took you " + game.gameRounds[game.gameRounds.length - 1].roll + " tries and Seconds)";
-      }
-      else if ((result[0] + result[1]) === 11) {
-        // console.log("Winner");
-        document.querySelector("h2").innerText = "Winner";
-      }
-      else {
-        document.querySelector("h2").innerText = "Try Again";
+          // // Tracking time to win
+          // game.gameRounds[game.gameRounds.length -1].time.getTime();
+          // console.log(game.gameRounds[game.gameRounds.length -1].time.getTime());
 
 
 
-      }
+        // Winner logic calculation
+        if ((result[0] + result[1]) === 7) {
+          // console.log("Winner");
+          document.querySelector("h2").innerText = "Winner";
+          document.querySelector(".message").innerText = "It took you " + game.gameRounds[game.gameRounds.length - 1].roll + " tries and Seconds)";
+        }
+        else if ((result[0] + result[1]) === 11) {
+          // console.log("Winner");
+          document.querySelector("h2").innerText = "Winner";
+        }
+        else {
+          document.querySelector("h2").innerText = "Try Again";
+        }
 
+      });
 
-
-
-
-
-
-      var now = new Date();
-      // console.log(now);
-      // console.log( now.getFullYear(), now.getMonth()+1, now.getDay(), now.getDate() );
-      // console.log(game.startDate);
-      // console.log(game.startDate - now);
-
-
-    });
-
-
-
-    var game = {
-      startDate: new Date,
-      gameRounds: [  { roll:0, time:new Date() }  ],
-      rollDice: function rollingDice () {
-        var firstDice = Math.ceil(Math.random() * 6);
-        var secondDice = Math.ceil(Math.random() * 6);
-        return [firstDice, secondDice];
-      }
-
-    };
-
-  })();
+    })();
